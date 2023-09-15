2f29bb0f00ff8b8af45aec551ff22f7efcdb89d5
Giminosk <giminosk@gmail.com>
2023-09-15 16:13:43 +0200

    added profile model and relation with user

---
+++ src/models/profile.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Date, ForeignKey
+
+from datetime import date
+from .base import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Profile(Base):
+    frirst_name: Mapped[str | None] = mapped_column(String(32))
+    last_name: Mapped[str | None] = mapped_column(String(32))
+    bio: Mapped[str | None] = mapped_column(String(500))
+    birthdate: Mapped[date | None] = mapped_column(Date)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="profile")
--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,14 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Boolean, DateTime
+from sqlalchemy.sql import func
 
+from datetime import datetime
 from .base import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .profile import Profile
 
 
 class User(Base):
@@ -10,8 +17,12 @@ class User(Base):
     username: Mapped[str | None] = mapped_column(String(32), unique=True)
     is_verified: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
 
-    # profile
+    profile: Mapped[Profile] = relationship(back_populates="user")
+
     # posts
     # comments
     # cart
